e868da819a6c04c05c3af34ebd5fa9fbb2b25e68
arkon <eugcheung94@gmail.com>
2020-04-04 10:10:00 -0400

    Perform local search when source is tapped from manga info

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaInfoController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaInfoController.kt
@@ -32,6 +32,7 @@ import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.notification.NotificationReceiver
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceManager
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.ui.base.controller.DialogController
@@ -54,6 +55,22 @@ import uy.kohesive.injekt.injectLazy
 import java.text.DateFormat
 import java.text.DecimalFormat
 import java.util.Date
+import kotlinx.android.synthetic.main.manga_info_controller.backdrop
+import kotlinx.android.synthetic.main.manga_info_controller.fab_favorite
+import kotlinx.android.synthetic.main.manga_info_controller.manga_artist
+import kotlinx.android.synthetic.main.manga_info_controller.manga_artist_label
+import kotlinx.android.synthetic.main.manga_info_controller.manga_author
+import kotlinx.android.synthetic.main.manga_info_controller.manga_chapters
+import kotlinx.android.synthetic.main.manga_info_controller.manga_cover
+import kotlinx.android.synthetic.main.manga_info_controller.manga_full_title
+import kotlinx.android.synthetic.main.manga_info_controller.manga_genres_tags
+import kotlinx.android.synthetic.main.manga_info_controller.manga_last_update
+import kotlinx.android.synthetic.main.manga_info_controller.manga_source
+import kotlinx.android.synthetic.main.manga_info_controller.manga_status
+import kotlinx.android.synthetic.main.manga_info_controller.manga_summary
+import kotlinx.android.synthetic.main.manga_info_controller.swipe_refresh
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 /**
  * Fragment that shows manga information.
@@ -196,10 +213,17 @@ class MangaInfoController : NucleusController<MangaInfoPresenter>(),
         }
 
         // If manga source is known update source TextView.
-        manga_source.text = if (source == null) {
-            view.context.getString(R.string.unknown)
-        } else {
-            source.toString()
+        val mangaSource = source?.toString()
+        with(manga_source) {
+            if (mangaSource != null) {
+                text = mangaSource
+                setOnClickListener {
+                    val sourceManager = Injekt.get<SourceManager>()
+                    performLocalSearch(sourceManager.getOrStub(source.id).name)
+                }
+            } else {
+                text = view.context.getString(R.string.unknown)
+            }
         }
 
         // Update genres list
