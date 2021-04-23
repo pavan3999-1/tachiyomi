f7545854df250798e1e7c36b4c5057a9e3a7c39d
pavan3999-1 <pavan143p4@gmail.com>
2021-04-23 15:11:53 +0530

    Update BrowseCatalogueController.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/browse/BrowseCatalogueController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/browse/BrowseCatalogueController.kt
@@ -299,7 +299,7 @@ open class BrowseCatalogueController(bundle: Bundle) :
      *
      * @param newQuery the new query.
      */
-    private searchWithQuery(newQuery: String) {
+    private fun searchWithQuery(newQuery: String) {
         // If text didn't change, do nothing
         if (presenter.query == newQuery)
             return
