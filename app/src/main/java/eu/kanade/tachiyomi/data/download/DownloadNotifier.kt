ouassim-boulkamh/amatsubu

[Feature Request] - Download Complete Remidner #3475 (#3527)
* [Feat] Show a download complete notification channel when all downloads are completed. Auto cancels when onclick and navigate to download screen.

* [Feat] Update the download message string to shorten the length.

app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadNotifier.kt
@@ -72,6 +72,27 @@ internal class DownloadNotifier(private val context: Context) {
     }
 
     /**
+     *  This function shows a notification to inform download tasks are done.
+     */
+    fun downloadFinished() {
+        // Create notification
+        with(notificationBuilder) {
+            setContentTitle(context.getString(R.string.download_notifier_downloader_title))
+            setContentText(context.getString(R.string.download_notifier_download_finish))
+            setSmallIcon(android.R.drawable.stat_sys_download_done)
+            clearActions()
+            setAutoCancel(true)
+            setContentIntent(NotificationHandler.openDownloadManagerPendingActivity(context))
+            setProgress(0, 0, false)
+        }
+        notificationBuilder.show(Notifications.ID_DOWNLOAD_CHAPTER_COMPLETE)
+
+        // Reset states to default
+        errorThrown = false
+        isDownloading = false
+    }
+
+    /**
      * Called when download progress changes.
      *
      * @param download download object containing download information.

pavan3999-1/tachiyomi

app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadNotifier.kt
@@ -90,6 +90,27 @@ internal class DownloadNotifier(private val context: Context) {
     }
 
     /**
+     *  This function shows a notification to inform download tasks are done.
+     */
+    fun downloadFinished() {
+        // Create notification
+        with(notification) {
+            setContentTitle(context.getString(R.string.download_notifier_downloader_title))
+            setContentText(context.getString(R.string.download_notifier_download_finish))
+            setSmallIcon(android.R.drawable.stat_sys_download_done)
+            clearActions()
+            setAutoCancel(true)
+            setContentIntent(NotificationHandler.openDownloadManagerPendingActivity(context))
+            setProgress(0, 0, false)
+        }
+        notification.show(Notifications.ID_DOWNLOAD_CHAPTER_COMPLETE)
+
+        // Reset states to default
+        errorThrown = false
+        isDownloading = false
+    }
+
+    /**
      * Called when download progress changes.
      * Note: Only accepted when multi download active.
      *