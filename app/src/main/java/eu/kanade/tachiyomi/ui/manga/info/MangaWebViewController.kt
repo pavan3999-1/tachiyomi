fd7dc0bc6d9c5ef02c747e196aa0e2328034d140
pavan3999-1 <pavan143p4@gmail.com>
2021-05-21 21:48:40 +0530

    Update MangaWebViewController.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaWebViewController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaWebViewController.kt
@@ -46,11 +46,13 @@ class MangaWebViewController(bundle: Bundle? = null) : BaseController(bundle) {
         web.settings.setAppCacheEnabled(true)
         web.settings.useWideViewPort = true
         web.settings.loadWithOverviewMode = true
-        web.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        web.settings.cacheMode(WebSettings.LOAD_DEFAULT)
         web.settings.setSupportZoom(true)
         web.settings.setBuiltInZoomControls(true)
         web.settings.setDisplayZoomControls(false)
         web.settings.setBuiltInZoomControls(true)
+        web.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY)
+        web.setScrollbarFadingEnabled(false)
         web.settings.userAgentString = source.headers["User-Agent"]
         web.loadUrl(url, headers)
     }
